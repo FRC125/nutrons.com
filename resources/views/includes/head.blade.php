95b5c8f2034973878d7d90628019758941034ca6
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2015-07-05 20:48:20 -0400

    added header and footer

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,22 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="description" content="NUTRONS FRC 125 Webiste - FIRST Robotics">
+<meta name="author" content="Camilo Gonzalez">
+
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" data-skrollr-stylesheet>
+<link href="{{ asset('assets/css/green.css') }}" rel="stylesheet" title="Color">
+<link href="{{ asset('assets/css/owl.carousel.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/owl.transitions.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/animate.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/fonts/fontello.css') }}" rel="stylesheet">
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+<link href="http://fonts.googleapis.com/css?family=Lato:400,900,300,700" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic" rel="stylesheet">
+
+<!-- HTML5 elements and media queries Support for IE8 : HTML5 shim and Respond.js -->
+<!--[if lt IE 9]>
+  <script src="{{ asset('assets/js/html5shiv.js') }}"></script>
+  <script src="{{ asset('assets/js/respond.min.js') }}"></script>
+<![endif]-->
